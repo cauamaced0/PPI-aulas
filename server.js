e283ef6969485b447e2d1599e39f8af3fc16f87a
cauamaced0 <cauassmacedo01@gmail..com>
2025-05-26 12:32:49 -0300

    npm

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const path = require('path');
+import express from 'express';
+import session from'express-session';
+import bodyparser from 'body-parser';
+import path from'path';
 
 const app = express();
 const PORT = 3050;
